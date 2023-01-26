6f4006746ff3a8452b4e3222236674de36ea10d3
JeffMota <jmota906@gmail.com>
2023-01-26 18:46:41 -0300

    feat: Lista mais vendidos static

---
+++ src/components/CardMaisVendido.js
@@ -0,0 +1,42 @@
+import styled from "styled-components"
+
+export default function CardMaisVendido({ name, imgURL }) {
+    return (
+        <MaisVendidoContainer>
+            <img src={imgURL} alt='Imagem Produto' />
+            <h2>{name}</h2>
+        </MaisVendidoContainer>
+    )
+}
+
+const MaisVendidoContainer = styled.div`
+    display: flex;
+    margin-left: 20px;
+    padding: 15px 0;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+
+    border-radius: 15px;
+    font-size: 16px;
+    color: #FFFF;
+
+    -webkit-text-stroke-width: 1px;
+    -webkit-text-stroke-color: #9E7972;
+
+    display: flex;
+    height: 162px;
+    min-width: 180px;
+
+    background-color: #F8C1C1;
+
+
+        >img{
+            width: 80%;
+            height: 70%;
+
+            border-radius: 15px;
+
+            object-fit: cover;
+        }
+`
